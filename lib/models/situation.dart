02a2f672df1aebfbc3e3e490567f832069b7c829
Sabri <s1157115@student.hsleiden.nl>
2026-03-23 14:26:43 +0100

    Feat: Model situation toegevoegd en situations om een structuur voor de situaties te hebben en de situaties nu in een lijst te kunnen maken

---
+++ lib/models/situation.dart
@@ -0,0 +1,20 @@
+class Situation {
+  final int id;
+  final String description;
+  final List <Choice> choices;
+
+  const Situation ({required this.id, required this.description, required this.choices});
+
+}
+
+class Choice{
+  final int id;
+  final String description;
+  final String outcome;
+
+
+  const Choice ({required this.id, required this.description, required this.outcome});
+
+}
+
+
