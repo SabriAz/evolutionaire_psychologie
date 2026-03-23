e6e150cecf841e5421ee42839a9b805e3308ad00
Sabri <s1157115@student.hsleiden.nl>
2026-03-23 14:10:19 +0100

    Feat: Explanation screen heeft nu een knop die je weer leidt naar het keuzescherm waardoor het nu een oneindige loop is tot het win- of verliescherm wordt gemaakt

--- lib/screens/explanationScreen.dart
+++ lib/screens/explanationScreen.dart
@@ -1,14 +1,27 @@
+import 'package:firebase_demo_test/screens/situationScreen.dart';
 import 'package:flutter/material.dart';
 
 class ExplanationScreen extends StatelessWidget {
   const ExplanationScreen({super.key});
   @override
-  Widget build(BuildContext build) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.red,
       ),
-      body: Text("Uitleg scherm"),
+      body: Column(
+        children: [
+          Text("Super goede uitleg..."),
+          ElevatedButton(
+            child: Text("Volgende"),
+            onPressed: () {
+            Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                    builder: (_) => SituationScreen()));
+          },)
+        ]
+      )
     );
   }
     
