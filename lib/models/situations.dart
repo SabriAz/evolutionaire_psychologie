fdd22c82b9939f83a5774c7fc022dfb12de45aab
Coen Scheer <s1151530@student.hsleiden.nl>
2026-03-26 11:41:02 +0100

    refactor: navigatie veranderd

--- lib/models/situations.dart
+++ lib/models/situations.dart
@@ -66,13 +66,10 @@ List <Situation> modern_situations = [
             id: 3,
             description: "Ren weg!",
             outcome: 2),
-        Choice(
-            id: 4,
-            description: "Ren heel hard weg!!",
-            outcome: 2)
+
       ],
       explanation: "Zoet voedsel was zeldzaam in de oertijd. Je brein ontwikkelde een sterke voorkeur voor zoet omdat het veel energie bevatte. Daarom is taart of snoep vandaag zo moeilijk te weerstaan — je brein reageert nog steeds alsof suiker zeldzaam is!",
-      situationImagePath: "assets/images/bear_situation.png",
+      situationImagePath: "assets/images/car_situation.png",
       explanationImagePath: "assets/images/bear_explanation.png"
   ),
   Situation(
@@ -93,7 +90,7 @@ List <Situation> modern_situations = [
             outcome: 3),
       ],
       explanation: "In de oertijd was energie sparen levensbelangrijk. Je brein beloont rust omdat beweging vroeger energie kostte die je nodig had voor jagen of vluchten. Daarom voelt de bank zo aantrekkelijk — je oerbrein denkt nog steeds dat je energie moet sparen!",
-      situationImagePath: "assets/images/bear_situation_2.png",
+      situationImagePath: "assets/images/car_situation.png",
       explanationImagePath: "assets/images/bear_explanation_2.png"
   )
 ];
