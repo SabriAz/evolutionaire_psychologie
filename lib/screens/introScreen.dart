9c831453175d239ada464ad2cb50af138da095f2
Sabri <s1157115@student.hsleiden.nl>
2026-04-16 10:55:47 +0200

    Fix: pushReplacement instead of push

--- lib/screens/introScreen.dart
+++ lib/screens/introScreen.dart
@@ -55,7 +55,7 @@ class IntroScreen extends StatelessWidget {
                     context,
                     "Start spel",
                         () {
-                      Navigator.push(
+                      Navigator.pushReplacement(
                         context,
                         MaterialPageRoute(builder: (context) => const HomeScreen()),
                       );
