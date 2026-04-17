161065a447a36b8b26bc1ba0005f5f638be22ee7
Sabri <s1157115@student.hsleiden.nl>
2026-04-17 12:25:26 +0200

    Feat: Added new file for intro screen

---
+++ lib/screens/introScreen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'modeSelectScreen.dart';
+
+class IntroScreen extends StatefulWidget {
+  const IntroScreen({super.key});
+
+  @override
+  State<IntroScreen> createState() => _IntroScreenState();
+}
